63fadd84f485736b6f98c63cb65fd260c31f07d7
guozengxin <guozengxin@outlook.com>
2014-07-07 21:27:27 +0800

    add vr forcequery tool

--- tool/urls.py
+++ tool/urls.py
@@ -7,4 +7,6 @@ urlpatterns = patterns(
     url(r'^urlbase64/$', views.urlbase64, name='urlbase64'),
     url(r'^base64-encode$', views.base64_encode, name='base64-encode'),
     url(r'^base64-decode$', views.base64_decode, name='base64-decode'),
+    url(r'^vr-forcequery$', views.vr_forcequery, name='vr-forcequery'),
+    url(r'^run-force$', views.run_force, name='run-force'),
 )
--- tool/views.py
+++ tool/views.py
@@ -3,7 +3,11 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.http import HttpResponse
+
 import json
+import urllib2
+import urllib
+import time
 
 from service import encoding
 
@@ -40,3 +44,31 @@ def base64_decode(request):
         response['info'] = '转换失败'
     response['result'] = result
     return HttpResponse(json.dumps(response))
+
+
+def vr_forcequery(request):
+    return render_to_response('tool/vr-forcequery.html', context_instance=RequestContext(request))
+
+
+def run_force(request):
+    vrBase = 'http://cnc.pic.sogou.com.z.sogou-op.org/pics/newxmlvr.jsp?rawQuery=%s&query=%s&forceQuery=on'
+    webBase = 'http://www.sogou.com/web?query=%s&ie=utf8&_ast=%d&_asf=null&forceQuery=on'
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36'}
+
+    query = request.POST.get('query', None)
+    query = urllib.quote(query.encode('utf-8'))
+    vrurl = vrBase % (query, query)
+    weburl = webBase % (query, int(time.time()))
+
+    ret = {'status': True, 'weburl': weburl, 'error': ''}
+    try:
+        req = urllib2.Request(vrurl, headers=headers)
+        response = urllib2.urlopen(req)
+        content = response.read()
+        time.sleep(1)
+        del content
+    except Exception, e:
+        ret['error'] = str(e)
+        ret['status'] = False
+    print ret
+    return HttpResponse(json.dumps(ret))
